2e306fa5135572c450ba0b300014b1d468a5970d
ishita12rawat <ishitarawat2004@gmail.com>
2024-07-25 16:16:17 +0530

    change

--- first.js
+++ first.js
@@ -1,18 +1,14 @@
-let http=require("http")
-let fs=require("fs");
-console.log("starting the server")
-http.createServer((req,res)=>{
-   
-    fs.readFile('index.html',(err,data)=>{
-        if(!err){
-       res.writeHead(200,{
-        "Content-Type":"text/html",
-       });
-       res.write(data);
-       return res.end();
-        }
-        res.write("error while reading"+err)
-        return res.end();
-    })
+//port specfic,interal addre
+//header=metadata,it taketwo argument(status,key value,
+//loopback address, //1loopbackaddre)
+//buffer are like array (fixed size)fixed length bit structure encoded contniously scanned
+//utf-8 used to convert string to n=binary ,stream = flow of data,streaming
+const express =require("express");
+const app=express();
+const PORT =3000;
+app.get('/',(req,res)=>{
+    res.end("hello world")
+});
+app.listen(PORT,()=>{
+    console.log(`example ${PORT}`)
 })
-.listen(8080);
